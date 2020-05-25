8f12bf22deadaa44df4b7cb9af6c2bd12bb243c4
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-25 21:10:05 +1000

    OOP. Add ToString method

--- CarManager/src/com/alexkozyura/carmanager/Car.java
+++ CarManager/src/com/alexkozyura/carmanager/Car.java
@@ -1,13 +1,13 @@
 package com.alexkozyura.carmanager;
 
 public class Car {
-	
+
 	String mark;
 	int yearOfProduction;
 	int price;
 	int weigth;
 	String color;
-	
+
 	public Car(String mark, int yearOfProduction, int price, int weigth, String color) {
 		this.mark = mark;
 		this.yearOfProduction = yearOfProduction;
@@ -16,5 +16,12 @@ public class Car {
 		this.color = color;
 	}
 
-	public Car(){}
+	public Car() {
+	}
+
+	@Override
+	public String toString() {
+		return "Car [mark=" + mark + ", yearOfProduction=" + yearOfProduction + ", price=" + price + ", weigth="
+				+ weigth + ", color=" + color + "]";
+	}
 }
