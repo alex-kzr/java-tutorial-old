d84b3adefdce5bae6acf49280503c670b1e3a8c2
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-25 21:09:33 +1000

    OOP. Add class constructors

--- CarManager/src/com/alexkozyura/carmanager/Car.java
+++ CarManager/src/com/alexkozyura/carmanager/Car.java
@@ -8,4 +8,13 @@ public class Car {
 	int weigth;
 	String color;
 	
+	public Car(String mark, int yearOfProduction, int price, int weigth, String color) {
+		this.mark = mark;
+		this.yearOfProduction = yearOfProduction;
+		this.price = price;
+		this.weigth = weigth;
+		this.color = color;
+	}
+
+	public Car(){}
 }
