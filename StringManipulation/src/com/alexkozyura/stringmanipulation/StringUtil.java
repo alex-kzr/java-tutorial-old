c9d7c898026fa9d53123006e9daf9720a5577b21
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-26 19:41:17 +1000

    Add for & while loops

--- StringManipulation/src/com/alexkozyura/stringmanipulation/StringUtil.java
+++ StringManipulation/src/com/alexkozyura/stringmanipulation/StringUtil.java
@@ -2,20 +2,27 @@ package com.alexkozyura.stringmanipulation;
 
 public class StringUtil {
 	public static void main(String[] args) {
-		
-		String source = "Hello World!";
-		
+
+		String source = "Hello my beautiful World!";
+
 		String[] words = source.split(" ");
-		
+
 		System.out.println(words.length);
-		System.out.println(words[0]);
-		
-		char[] letters1 = words[0].toCharArray();		
-		System.out.println(letters1.length);
-		
-		System.out.println(words[1]);
-		
-		char[] letters2 = words[1].toCharArray();		
-		System.out.println(letters2.length);
+
+		for (int i = 0; i < words.length; i++) {
+
+			System.out.println(words[i]);
+			char[] letters = words[i].toCharArray();
+			System.out.println(letters.length);
+		}
+
+		int i = 0;
+		while (i < words.length) {
+			
+			System.out.println(words[i]);
+			char[] letters = words[i].toCharArray();
+			System.out.println(letters.length);
+			i++;
+		}
 	}
 }
