9b86cd52802d1a0ea16260d0b6c67290df618c74
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-26 20:00:12 +1000

    Add lists

--- CarManager/src/com/alexkozyura/carmanager/CarManager.java
+++ CarManager/src/com/alexkozyura/carmanager/CarManager.java
@@ -1,22 +1,34 @@
 package com.alexkozyura.carmanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarManager {
 
 	public static void main(String[] args) {
 
 		PassengerCar prius = new PassengerCar("Toyota", 2008, 15000, 1200, Color.GREY);
 
-		prius.addDistance(15000);
-		
 		Bus laguna = new Bus("Renault", 2000, 9700, 2000, Color.WHITE);
+		
+		PassengerCar prius2 = new PassengerCar("Toyota", 2008, 15000, 1200, Color.GREY);
+		
+		List<Car> cars = new ArrayList<>();
+
+		cars.add(prius);
+		cars.add(laguna);
+		cars.add(prius2);
+		
+		System.out.println(cars);
+		
+		prius.addDistance(15000);
 		
 		prius.addDistance(15000);
 		
 		System.out.println(prius);
 		System.out.println(laguna);
 
-		PassengerCar prius2 = new PassengerCar("Toyota", 2008, 15000, 1200, Color.GREY);
-
+		
 		System.out.println(prius.equals(prius2));
 		System.out.println(prius.equals(laguna));
 
